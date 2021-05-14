67e8cc6db551870c96396b3605016b915ff0019e
Yermek Kapushev <kapushev@gmail.com>
2021-05-14 17:50:02 +0300

    Obligation now contains abstract syntax tree

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="coq_serapy",
+    version="0.1",
+    author="",
+    description="Python interface for interacting with coq-serapi",
+    packages=["coq_serapy"],
+    python_requires=">=3.8",
+    requires=["pampy", "sexpdata", "tqdm"]
+)
